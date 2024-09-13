95d9298e4046a04d0481fd5444e0faf3f9c86c07
BREINA <breinapaige@gmail.com>
2024-09-12 20:31:11 -0400

    finished learning comments!

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,21 +1,33 @@
 /**
- * Function Calls
+ * The Blank Page
  * Breina Kelly
  * 
- * Working on the function call examples
+ * Working on the function call examples, these comments 
+ * explain what our projects about
+ * 
+ * An exploration of the existential angst of a novelist
+ * who must sit down at their pink desk and coonfront the
+ * abyss that is a blank page of paper
+ * 
+ * The program is non-interactive to conveey the inability
+ * toget started on the prject. Try typing! Just try!
  */
 
 "use strict";
 
 
 function setup() {
- //once at the beginning of the program
+//these are instruction type comments
+//once at the beginning of the program
+//create a canvas at a standard resolution
  createCanvas(640,480);
 }
 
 
 function draw() {
 //every frame
+//the pink desk
 background(255,100,100);
-Reflect(200,80,240,320);
+//the blank piece of paper
+rect(200,80,240,320);
 }
